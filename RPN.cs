ce8cd20ff039bacef02da4d4f2039cb037ac8a4d
Slava <nedavni@gmail.com>
2019-09-27 12:49:06 +0300

    Improving

--- RPN.cs
+++ RPN.cs
@@ -8,25 +8,34 @@ namespace Reverse_Polish_Notation
 {
     public class RPN
     {
+        private readonly List<string> _allOperations;
+        private readonly List<string> _brackets;
+
+        public RPN()
+        {
+            _allOperations==new List<string>(){"/","-","+","*","^"};
+            _brackets = new List<String>() { "(", ")" };
+        }
+
         public double Calculate(string expression)
         {
             List<double> numbers = new List<double>();
             List<string> operations = new List<string>();
-            bool isOpeningBracket = false;
+            string previousSymbol="";
 
             for(int i=0;i<expression.Length;i++)
             {
                 string symbol= expression[i].ToString();
 
-                if(symbol=="(")
-                {
-                    isOpeningBracket = true;
-                }
-
-                if(i%2==0&&!isOpeningBracket||isOpeningBracket&& i % 2 != 0)
+                if(!_allOperations.Contains(symbol)&&!_brackets.Contains(symbol))
                 {
                     try
                     {
+                        if (!_allOperations.Contains(previousSymbol) && _brackets.Contains(previousSymbol) &&
+                            previousSymbol != "")
+                        {
+                            symbol = previousSymbol + symbol;
+                        }
                         numbers.Add(Convert.ToDouble(symbol));
                         continue;
                     }
@@ -34,70 +43,76 @@ namespace Reverse_Polish_Notation
                     {
                         throw new Exception("Wrong expression");
                     }
-                }
+                }//adding numbers
 
                 else
                 {
-                    string currentOperation = symbol;
-
-                    if (symbol != "/" && symbol != "(" && symbol != ")" && symbol != "-" &&
-                        symbol != "+" && symbol != "*" && symbol != "^")
+                    if (_allOperations.Contains(previousSymbol))
                     {
                         throw new Exception("Wrong expression");
-                    }
+                    }//if previous symbol is operation too
 
                     else
                     {
-                        if(operations.Count==0||Prioritization(operations.Last<string>(),symbol)==1)
+                        if(Prioritization(operations.Last<string>(),symbol)==1)
                         {
                             operations.Add(symbol);
-                        }
+                        }//adding operation,if it's preoritization==1
+
                         else
                         {
                             if (symbol != ")")
                             {
-                                CalculateResultOfAnOperation(ref operations, ref numbers);
+                                numbers.Add(CalculateResultOfAnOperation(operations.Last(),numbers[numbers.Count-2],numbers.Last()));
+                                numbers.RemoveRange(numbers.Count - 3, 2);
+                                operations.RemoveAt(operations.Count - 1);
                                 operations.Add(symbol);
-                            }
+                            }//calculate operation
+
                             else
                             {
                                 while (operations.Last<string>() != "(")
                                 {
-                                     CalculateResultOfAnOperation(ref operations, ref numbers);
+                                    numbers.Add(CalculateResultOfAnOperation(operations.Last(), numbers[numbers.Count - 2], numbers.Last()));
+                                    numbers.RemoveRange(numbers.Count - 3, 2);
+                                    operations.RemoveAt(operations.Count - 1);
                                 }
+
                                 operations.RemoveAt(operations.Count - 1);
-                                isOpeningBracket = false;
-                            }
+                            }//calculate while operation!=(
 
                         }
                     }
                 }
 
-            }
+                previousSymbol = symbol;
+
+            }//expression's symbol parse into operation or number
 
             while(numbers.Count!=1)
             {
-                CalculateResultOfAnOperation(ref operations, ref numbers);
-            }
+                numbers.Add(CalculateResultOfAnOperation(operations.Last(), numbers[numbers.Count - 2], numbers.Last()));
+                numbers.RemoveRange(numbers.Count - 3, 2);
+                operations.RemoveAt(operations.Count - 1);
+            }//if operations aren't ended, calculations
 
-                return numbers[0];
+            return numbers[0];
         }
 
-        private int Prioritization(string lastOperation,string operation)
+        private int Prioritization(string lastOperation="",string operation)
         {
                     if (((lastOperation == "-" || lastOperation == "+") &&
                         (operation == "/" || operation == "^" || operation == "*"))
-                        || operation == "("|| lastOperation == "(")
+                        || operation == "("|| lastOperation == "("||lastOperation=="")
                     {
                         return 1;
                     }
                     return 0;
         }
-        private void CalculateResultOfAnOperation(ref List<string> operations,ref List<double> numbers)
+
+        private double CalculateResultOfAnOperation(string operation,double firsNumber,double secondNumber)
         {
             double result = 0;
-            double firstNumber = numbers[numbers.Count - 2];
-            double secondNumber = numbers.Last<double>();
 
             switch (operations.Last<string>())
             {
@@ -122,9 +137,7 @@ namespace Reverse_Polish_Notation
                     break;
             }
 
-            numbers.RemoveRange(numbers.Count - 2, 2);
-            numbers.Add(result);
-            operations.RemoveAt(operations.Count - 1);
+            return result;
         }
     }
 }
